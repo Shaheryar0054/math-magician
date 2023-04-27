35000112bedcb32018976f640448c13ad681ba9f
shaheryar abid <sharyar0310@gmail.com>
2023-04-27 19:15:18 +0500

    Update Quote.js

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import './calculator.css';
+import './Calculator.css';
 
 const Quote = ({ category }) => {
   const [quote, setQuote] = useState('');
